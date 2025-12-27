c379c8aa2faa177ebb9db5c8f8ca3d6624004594
Bahija El hajali <bahija.elhajali.96@edu.uiz.ac.ma>
2025-12-27 01:47:57 +0100

    Finalisation du merge -réorganisation de la structure de projet SuperMaroc

--- superMaroc-api/src/main/java/repository/CommandeRepository.java
+++
@@ -1,6 +0,0 @@
-package com.SuperMaroc.repository;
-
-import com.SuperMaroc.model.Commande;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommandeRepository extends JpaRepository<Commande, Long> { }
